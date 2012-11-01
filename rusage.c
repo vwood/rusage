ee16c3ac788dbc96f2a76460fb86f2644cd1e3a2
VWood <vwood.org@gmail.com>
2012-11-01 16:40:31 +1300

    Fix argument passing

--- rusage.c
+++ rusage.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv) {
 
     pid = fork();
     if (pid == 0) {
-        execv(argv[1], argv + 2);
+        execv(argv[1], argv + 1);
         return 1;
     } else if (pid < 0) {
         return 1;
